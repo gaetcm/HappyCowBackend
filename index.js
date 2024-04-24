c4aabcc747204c4ef0fcede3f6b72c190d3e0a53
gaetan <courtemanche.gaetan@gmail.com>
2024-04-24 17:22:24 +0200

    filtres

--- index.js
+++ index.js
@@ -10,12 +10,23 @@ app.use(express.json());
 app.get("/restaurants", cors(), async (req, res) => {
   try {
     const { name = "", skip = "", limit = "" } = req.query;
-
     const response = await axios.get(
-      `https://res.cloudinary.com/lereacteur-apollo/raw/upload/v1575242111/10w-full-stack/Scraping/restaurants.json?name=${name}&skip=${skip}&limit=${limit}`
+      `https://res.cloudinary.com/lereacteur-apollo/raw/upload/v1575242111/10w-full-stack/Scraping/restaurants.json`
+    );
+    const filteredRestaurants = response.data.filter((restaurant) =>
+      restaurant.name.toLowerCase().includes(name.toLowerCase())
     );
 
-    res.json(response.data);
+    // skip and limit
+    const startIndex = parseInt(skip) || 0;
+    const endIndex =
+      startIndex + (parseInt(limit) || filteredRestaurants.length);
+    const paginatedRestaurants = filteredRestaurants.slice(
+      startIndex,
+      endIndex
+    );
+
+    res.json(paginatedRestaurants);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -35,6 +46,49 @@ app.get("/restaurants/:id", cors(), async (req, res) => {
       return res.status(404).json({ message: "Restaurant not found" });
     }
     res.json(restaurant);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/carte", cors(), async (req, res) => {
+  try {
+    const {
+      name = "",
+      skip = "",
+      limit = "",
+      category = "",
+      vegan = "",
+    } = req.query;
+    const response = await axios.get(
+      `https://res.cloudinary.com/lereacteur-apollo/raw/upload/v1575242111/10w-full-stack/Scraping/restaurants.json`
+    );
+    const filteredRestaurants = response.data.filter((restaurant) =>
+      restaurant.name.toLowerCase().includes(name.toLowerCase())
+    );
+
+    // Filtrage par catégorie
+    const categoryFilter = parseInt(category);
+    const filteredByCategory = categoryFilter
+      ? filteredRestaurants.filter((restaurant) =>
+          restaurant.category.includes(categoryFilter)
+        )
+      : filteredRestaurants;
+
+    const veganFilter = parseInt(vegan);
+    const filteredByVegan = veganFilter
+      ? filteredRestaurants.filter((restaurant) =>
+          restaurant.vegan.includes(veganFilter)
+        )
+      : filteredRestaurants;
+
+    // skip and limit
+    const startIndex = parseInt(skip) || 0;
+    const endIndex =
+      startIndex + (parseInt(limit) || filteredByCategory.length);
+    const paginatedRestaurants = filteredByCategory.slice(startIndex, endIndex);
+
+    res.json(paginatedRestaurants);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
