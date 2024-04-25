e83e685ebc4e359c4b9f30cae6d75cd23ae71742
gaetan <courtemanche.gaetan@gmail.com>
2024-04-25 15:03:41 +0200

    commit

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const axios = require("axios");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-// const mongoose =require("mongoose")
 
-// Requête restaurant :
 app.get("/restaurants", cors(), async (req, res) => {
   try {
     const { name = "", skip = "", limit = "" } = req.query;
@@ -17,7 +15,6 @@ app.get("/restaurants", cors(), async (req, res) => {
       restaurant.name.toLowerCase().includes(name.toLowerCase())
     );
 
-    // skip and limit
     const startIndex = parseInt(skip) || 0;
     const endIndex =
       startIndex + (parseInt(limit) || filteredRestaurants.length);
@@ -32,7 +29,6 @@ app.get("/restaurants", cors(), async (req, res) => {
   }
 });
 
-// Requête resto par ID
 app.get("/restaurants/:id", cors(), async (req, res) => {
   const id = parseInt(req.params.id);
   try {
@@ -63,30 +59,31 @@ app.get("/carte", cors(), async (req, res) => {
     const response = await axios.get(
       `https://res.cloudinary.com/lereacteur-apollo/raw/upload/v1575242111/10w-full-stack/Scraping/restaurants.json`
     );
-    const filteredRestaurants = response.data.filter((restaurant) =>
+    let filteredRestaurants = response.data.filter((restaurant) =>
       restaurant.name.toLowerCase().includes(name.toLowerCase())
     );
 
-    // Filtrage par catégorie
     const categoryFilter = parseInt(category);
-    const filteredByCategory = categoryFilter
-      ? filteredRestaurants.filter((restaurant) =>
-          restaurant.category.includes(categoryFilter)
-        )
-      : filteredRestaurants;
+    if (categoryFilter) {
+      filteredRestaurants = filteredRestaurants.filter((restaurant) =>
+        restaurant.category.includes(categoryFilter)
+      );
+    }
 
     const veganFilter = parseInt(vegan);
-    const filteredByVegan = veganFilter
-      ? filteredRestaurants.filter((restaurant) =>
-          restaurant.vegan.includes(veganFilter)
-        )
-      : filteredRestaurants;
+    if (veganFilter) {
+      filteredRestaurants = filteredRestaurants.filter((restaurant) =>
+        restaurant.vegan.includes(veganFilter)
+      );
+    }
 
-    // skip and limit
     const startIndex = parseInt(skip) || 0;
     const endIndex =
-      startIndex + (parseInt(limit) || filteredByCategory.length);
-    const paginatedRestaurants = filteredByCategory.slice(startIndex, endIndex);
+      startIndex + (parseInt(limit) || filteredRestaurants.length);
+    const paginatedRestaurants = filteredRestaurants.slice(
+      startIndex,
+      endIndex
+    );
 
     res.json(paginatedRestaurants);
   } catch (error) {
